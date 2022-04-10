3b5c60e657ad63458ca7b904acc8b66e067af077
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-04-09 21:21:41 -0400

    Create basic api

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,34 @@
 const result = document.querySelector('.result')
+
+const fetchData = async (event, context) => {
+  try {
+    const { data } = await axios.get('/api/2-basic-api')
+    // console.log(data.map((item) => item.name))
+    const products = data
+      .map((product) => {
+        const {
+          image: { url },
+          name,
+          price
+        } = product
+        return /*html*/ `
+        <article class="product">
+          <img
+            src="${url}"
+            alt="${name}"
+          />
+          <div class="info">
+            <h5>${name}</h5>
+            <h5 class="price">$${price}</h5>
+          </div>
+      </article>
+      `
+      })
+      .join('')
+    result.innerHTML = products
+  } catch (error) {
+    console.log(error.response)
+  }
+}
+
+fetchData()
